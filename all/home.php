11c5931a066825d71fb4e18bbedaa66dec0ff6fe
Nickolay Grekalo <64259058+Grekoff18@users.noreply.github.com>
2020-08-16 17:19:26 +0300

    First steps

---
+++ all/home.php
@@ -0,0 +1,18 @@
+<?php top("Главная страница"); ?>
+
+<div class="wrapper">
+	<div class="slide">
+		<div><p>Здесь могла быть ваша реклама</p></div>
+		<div><p>Здесь могла быть ваша реклама</p></div>
+		<div><p>Здесь могла быть ваша реклама</p></div>
+	</div>
+	<div class="content"></div>
+	<div class="slide">
+		<div><p>Здесь могла быть ваша реклама</p></div>
+		<div><p>Здесь могла быть ваша реклама</p></div>
+		<div><p>Здесь могла быть ваша реклама</p></div>
+	</div>
+</div>
+
+<?php bottom(); ?>
+
--- index.php
+++ index.php
@@ -6,22 +6,34 @@ if ($_SERVER['REQUEST_URI'] == '/') {
 }
 
 session_start();
+require_once("config.php");
 
-if (file_exists('all/$page.php')) {
-    include 'all/$page.php';
-} else if ($_SESSION['id'] == 1 and file_exists('auth/$page.php')) {
-    include 'auth/$page.php';
-} else if ($_SESSION['id'] != 1 and file_exists('guest/$page.php')) {
-    include 'guest/$page.php';
+// The usual distribution of site pages
+if (file_exists("all/$page.php")) {
+    require_once("all/$page.php");
+} else if ($_SESSION['id'] == 1 and file_exists("auth/$page.php")) {
+    require_once("auth/$page.php");
+} else if ($_SESSION['id'] != 1 and file_exists("guest/$page.php")) {
+    require_once("guest/$page.php");
 } else {
     exit('Page not found 404');
 }
-
+// Connecting to database
 function db() {
     global $db;
-    $db = mysql_connect();
-    if ($db) {
-        exit('Error database connection((');
+    $db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if (!$db) {
+        exit('Error database connection');
     }
 }
+db();
+// This is not a header because the header is already taken by the standard php function 
+function top($title) {
+    require_once("sections/top.php");
+};
+//Then I decided to continue to call parts of the site by common names.
+function bottom() {
+    require_once("sections/bottom.php");
+};
+
 ?>
