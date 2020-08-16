0a3c817c2bbaf07441a421d89b771ff3d6cddbbb
Nickolay Grekalo <64259058+Grekoff18@users.noreply.github.com>
2020-08-16 19:36:37 +0300

    2st-commit
    A registration page with google captcha has been created but not completed to the end. Also, a login page was created and errors of past work were fixed.

--- sections/top.php
+++ sections/top.php
@@ -6,4 +6,11 @@
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
+	<div class="wrapper">
+		<div class="slide">
+			<div><p>Здесь могла быть ваша реклама</p></div>
+			<div><p>Здесь могла быть ваша реклама</p></div>
+			<div><p>Здесь могла быть ваша реклама</p></div>
+		</div>
+		<div class="content">
 
